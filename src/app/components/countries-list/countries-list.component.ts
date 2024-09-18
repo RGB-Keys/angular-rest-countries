a655d93ab832332045d980d53657f12ce4fd6034
RGB-Keys <ramongtstudy@gmail.com>
2024-09-18 15:22:52 -0300

    adding search-box

--- src/app/components/countries-list/countries-list.component.ts
+++ src/app/components/countries-list/countries-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, switchMap, tap } from 'rxjs';
 import { Country } from 'src/app/models/countries/countries.module';
-import { CountryService } from 'src/app/services/country.service';
+import { CountryService } from 'src/app/services/country-api/country.service';
 
 
 @Component({
@@ -8,27 +9,45 @@ import { CountryService } from 'src/app/services/country.service';
   templateUrl: './countries-list.component.html',
   styleUrls: ['./countries-list.component.css']
 })
-export class CountriesListComponent {
-  countries: Country[] = [];
+export class CountriesListComponent implements OnInit {
+  countries$: Observable<Country[]>;
+  totalCountries$: Observable<number>;
   currentPage = 1;
   pageSize = 12;
   totalCountries = 0;
 
-  constructor(private countryService: CountryService) {}
+  constructor(private countryService: CountryService) {
+    this.countries$ = this.countryService.getPaginatedAndFilteredCountries();
+    this.totalCountries$ = this.countryService.getTotalFilteredCountries();
+  }
 
   ngOnInit(): void {
     this.loadCountries();
+    this.onSearch();
   }
 
   loadCountries(): void {
-    this.countryService.getCountries().subscribe((countries) => {
-      this.totalCountries = countries.length;
-      this.countryService.getPaginatedCountries(this.currentPage, this.pageSize)
-      .subscribe(paginatedCountries => {
-        this.countries = paginatedCountries;
-      });
+    this.OnPage();
+  }
+
+  onSearch(): void {
+    this.countryService.getSearchTerm().subscribe(searchTerm => {
+      this.applyFilter(searchTerm);
     });
   }
+
+  applyFilter(searchTerm: string): void {
+    this.currentPage = 1;
+    this.loadCountries();
+  }
+
+  OnPage(): void {
+    this.countryService.setPage(this.currentPage);
+    this.countryService.setPageSize(this.pageSize);
+    this.countryService.getTotalFilteredCountries().subscribe(total => {
+      this.totalCountries = total;
+    });
+  } 
 
   onPageChange(page: number): void {
     this.currentPage = page;
